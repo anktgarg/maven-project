26ec3e58668d29d3025cd1be22f153208c865248
anktgarg <ankitgarg6030@gmail.com>
2020-04-04 14:53:08 +0530

    update

--- terrafiles/variables.tf
+++ terrafiles/variables.tf
@@ -2,4 +2,4 @@ variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-west-2"
 }
-variable "BUILD_NUMBER" { default = "${env.BUILD_NUMBER}" }
+#variable "BUILD_NUMBER" { default = "${env.BUILD_NUMBER}" }
